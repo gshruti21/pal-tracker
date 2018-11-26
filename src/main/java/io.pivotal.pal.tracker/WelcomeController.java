413b71bf92e795a90e5ed607a43712181b823ac0
Gupta <Shruti.Gupta1@emc.com>
2018-11-26 15:35:11 +0530

    gradle files

---
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class WelcomeController {
+
+    @Value("${WELCOME_MESSAGE}")
+    private String message;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+}
